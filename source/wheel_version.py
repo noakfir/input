639a2a1b6529d1bc7cfa6d841f2930670af3e521
noakfir <95751658+noakfir@users.noreply.github.com>
2022-04-27 10:45:55 +0300

    contains

--- source/wheel_version.py
+++ source/wheel_version.py
@@ -3,7 +3,7 @@ with open("setup.py", "r") as f:
     contents = f.readlines() 
 
 for word in contents:
-    if word.startswith("    version="):
+    if word.contains("version"):
         result = re.split("'+", word)
         print(result[1])
         break
