81ee3fb9e9b4aaa42e558916da5ce3293291ede4
noakfir <95751658+noakfir@users.noreply.github.com>
2022-04-27 10:57:16 +0300

    new

--- source/wheel_version.py
+++ source/wheel_version.py
@@ -3,7 +3,7 @@ with open("setup.py", "r") as f:
     contents = f.readlines() 
 
 for word in contents:
-    if word.contains("version"):
+    if "version" in word:
         result = re.split("'+", word)
         print(result[1])
         break
