0df4653759db68cf0a57fcb33ed739691371bf74
noakfir <95751658+noakfir@users.noreply.github.com>
2022-10-26 11:00:47 +0300

    changes

--- source/wheel_version.py
+++
@@ -1,9 +0,0 @@
-import re
-with open("setup.py", "r") as f:
-    contents = f.readlines() 
-
-for word in contents:
-    if "version" in word:
-        result = re.split("'+", word)
-        print(result[1])
-        break  
